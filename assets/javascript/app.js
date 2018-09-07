792a022aa1b1f5d342817823047afe429feea56e
Lewis Gregerson-Spencer <lewisgregerson-spencer@Lewiss-MacBook.local>
2018-09-07 11:18:18 -0700

    got the timer working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,11 @@
-let number = 300;
+/*let number = 300;
 let intervalID;
 
-$("#start").on("click", run)
+$("#begin").on("click", run)
 
-function run() {
+$(index.html).ready (function run() {
     intervalId = setInterval(decrement, 1000);
-  }
+  });
 
   function decrement() {
 
@@ -22,10 +22,36 @@ function run() {
   function stop() {
 
     clearInterval(intervalId);
+  }*/
+
+  document.getElementById('clock').innerHTML = 05 + ":" + 00;
+startTimer();
+
+function startTimer() {
+  var presentTime = document.getElementById('clock').innerHTML;
+  var timeArray = presentTime.split(/[:]+/);
+  var m = timeArray[0];
+  var s = checkSecond((timeArray[1] - 1));
+  if(s==59){
+    m=m-1;
+  }
+  if(m==0 && s==0){redirect();}
+
+  function redirect() {
+    window.location="file:///Users/lewisgregerson-spencer/Desktop/trivia-game/results.html"
+  }
+  
+  document.getElementById('clock').innerHTML =
+    m + ":" + s;
+  setTimeout(startTimer, 1000);
+}
+
+function checkSecond(sec) {
+  if (sec < 10 && sec >= 0) {sec = "0" + sec}; // add zero in front of numbers < 10
+  if (sec < 0) {sec = "59"};
+  return sec;
   }
 
-  $("#clock").hide();
 
-  $("#questions").hide();
 
-  $("#begin").on("click", show());
+  
