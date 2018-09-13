9731a724284be5ce5ce8ad8f83571b342706ed5b
Lewis Gregerson-Spencer <lewisgregerson-spencer@D-10-19-118-189.dhcp4.washington.edu>
2018-09-12 18:34:48 -0700

    final version

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# trivia-game
+Welcome to the Office Trivia Game!
+
+You have 5 minutes to answer 20 questions about the Office TV show. After five minutes (or after you hit submit) your score will be calculated. Any unanswered questions will be counted as incorrect. Get ready, the questions are in order of increasing difficulty.
+
+There is also one question with two correct answers. I'll let you figure out which one.
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,10 @@
-  document.getElementById('clock').innerHTML = 05 + ":" + 00;
+function hideResults() {
+  $("#resultspage").hide();
+}
+
+hideResults();
+
+document.getElementById('clock').innerHTML = 05 + ":" + 00;
 startTimer();
 
 function startTimer() {
@@ -10,7 +16,7 @@ function startTimer() {
     m=m-1;
   }
   if(m==0 && s==0){
-    redirect();
+     timesUp();
   }
   
   document.getElementById('clock').innerHTML =
@@ -24,35 +30,49 @@ function checkSecond(sec) {
   return sec;
   }
 
-  function redirect() {
-    window.location="file:///Users/lewisgregerson-spencer/Desktop/trivia-game/results.html"
-  }
+  // function redirect() {
+  //   window.location="file:///Users/lewisgregerson-spencer/Desktop/trivia-game/results.html"
+  // }
 
-  
+
 
 
   var numberCorrect = 0;
 
-function checkAnswers() { 
-var yup = $("input");
-var answerButtons = Array.from(yup)
-console.log(answerButtons)
-for(i=0; i<answerButtons.length; i++) {
-  var button = answerButtons[i];
-  //console.log(button.value)
-  //console.log(button.checked)
-  
-  if (button.value == "correct") {
-    console.log('found one')
-    numberCorrect++;
+  function checkAnswers() { 
+    var yup = $("input");
+    var answerButtons = Array.from(yup);
+    for(i=0; i<answerButtons.length; i++) {
+      let button = answerButtons[i];
+      
+      //this check was running when the page loads, so no button could be checked. that's why this conditional always failed.
+      if (button.checked == true && button.value == "correct") {
+        console.log('found one')
+        numberCorrect++;
+        }
+      }
+      $("#correctans").text("Correct: " + numberCorrect);
+      $("#incorrectans").text("Incorrect: " + (20 - numberCorrect));
     }
-  }
-  $("#correctans").text("Correct: " + numberCorrect);
-  console.log(numberCorrect);
-}
 
-$("#submit").on("click", checkAnswers())
+    //**********************/
 
+    //Instead of running the quizOver function when the page loads, we only want to check the answers when we actually hit submit.
 
+    //ADDED a click listener for that submit button
 
-  
+    $('#submit').on("click", quizOver);
+
+    function quizOver() {
+
+      //instead of click listeners in the quizOver function, just run the answer check, and swap the pages over. Since this function was being called when the page loads, the answers were being checked before a user had the chance to click anything
+      checkAnswers()
+      $("#resultspage").show();
+      $("#questionspage").hide();
+    }
+
+    // Your original invocation of this function. Would run as the page is loaded. We only want to do that when we submit. This way, a user can check some of the radio buttons.
+    // quizOver()
+
+    
+
